f33cfae9edf8a15bf342cd1cd65db9f0432a121a
ivenaccip <A01653722@itesm.mx>
2023-02-26 18:35:11 -0600

    Terminado

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # * ex: /polls/
     path('', views.index, name="index"),
     # * ex: /polls/3
-    path('<int:question_id>/asdasdasd', views.detail, name="detail"),
+    path('<int:question_id>/detail/', views.detail, name="detail"),
     # * ex: /polls/3/results
     path('<int:question_id>/results/', views.results, name="results"),
     # * ex: /polls/3/vote
